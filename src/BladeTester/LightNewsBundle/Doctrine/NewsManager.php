06ef48658756a97a2350940a21248fb9882fcc16
Fran Moreno <franmomu@gmail.com>
2013-02-28 21:11:01 +0100

    Refactor Newsmanager

--- src/BladeTester/LightNewsBundle/Doctrine/NewsManager.php
+++ src/BladeTester/LightNewsBundle/Doctrine/NewsManager.php
@@ -7,77 +7,71 @@ use BladeTester\LightNewsBundle\Model\NewsInterface;
 
 class NewsManager
 {
-    protected $serviceContainer;
+    protected $objectManager;
     protected $class;
+    protected $repository;
 
-    public function __construct($service_container, $class)
+    public function __construct(ObjectManager $object_manager, $class)
     {
-        $this->serviceContainer = $service_container;
-        $metadata = $this->getObjectManager()->getClassMetadata($class);
+        $this->objectManager = $object_manager;
+        $this->repository = $object_manager->getRepository($class);
+
+        $metadata = $object_manager->getClassMetadata($class);
         $this->class = $metadata->getName();
     }
-
 
     public function create($title = '', $body = '') {
         $news = $this->build($title, $body);
         $this->persist($news);
+
         return $news;
     }
 
     public function build($title = '', $body = '') {
-        $news = clone($this->serviceContainer->get('blade_tester_light_news.entity.news'));
+        $news = new $this->class;
         $news->setTitle($title);
         $news->setBody($body);
+
         return $news;
     }
 
     public function update(NewsInterface $news) {
         $news->setUpdatedAt(new \DateTime);
-        $this->getObjectManager()->flush();
+        $this->objectManager->flush();
+
         return $news;
     }
 
     public function persist(NewsInterface $news) {
-        $om = $this->getObjectManager();
-        $om->persist($news);
-        $om->flush();
+        $this->objectManager->persist($news);
+        $this->objectManager->flush();
+
         return $news;
     }
 
     public function remove(NewsInterface $news)
     {
-        $om = $this->getObjectManager();
-        $om->remove($news);
-        $om->flush();
+        $this->objectManager->remove($news);
+        $this->objectManager->flush();
+
         return $news;
     }
 
     public function refresh(NewsInterface $news)
     {
-        $om = $this->getObjectManager();
-        $om->refresh($news);
         return $news;
     }
 
     public function find($id) {
-        return $this->getRepository()->find($id);
+        return $this->repository->find($id);
     }
 
     public function findAll() {
-        return $this->getRepository()->findAll();
+        return $this->repository->findAll();
     }
 
     public function getClass()
     {
         return $this->class;
     }
-
-    public function getRepository() {
-        return $this->getObjectManager()->getRepository($this->class);
-    }
-
-    private function getObjectManager() {
-        return $this->serviceContainer->get('doctrine.orm.entity_manager');
-    }
-
 }
